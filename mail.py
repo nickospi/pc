d9baa6be8151dc2dcef3e33c7b3f7d50341f7cad
Nikos Pitsiladis <39416389+nickospi@users.noreply.github.com>
2021-07-02 22:38:31 +0300

    Update mail.py

--- mail.py
+++ mail.py
@@ -15,5 +15,6 @@ msgRoot['From'] = "npitsiladis@megatv.com"
 msgRoot['To'] = "nickospi@gmail.com"
 html ='Dear Sir/Madam, sterial found on your website and has never granted any license to the uploader. Thus, we demand immediate removal_list_ep)+nI state that upon a good faith belief the disputed use of the material or activity is not authorized by the copyright or intellectual property owner, its agent or the law and under penalty of perjury, ANT1 TV is the copyright or intellectual property owner or is authorized to act on behalf of the copyright or intellectual property owner and that the information provided in the notice is accurate.'+'\nNikos Pitsiladis Digital Manager \nKifissias 10-12 Maroussi \nThank you'
 #html = MIMEText(open('template.html', 'r').read(), "html", "utf-8")
-msgRoot.attach(html)
-smtp.sendmail("npitsiladis@megatv.com", "nickospi@gmail.com", msgRoot())
+part1 = MIMEText(html, 'html')
+msgRoot.attach(part1)
+smtp.sendmail("npitsiladis@megatv.com", "nickospi@gmail.com", msgRoot.as_string())
